06f1f079544f6121d505fdf06044a20b1fc52df9
2doh <ldh0973@naver.com>
2025-10-27 18:52:35 +0900

    docs : 메인페이지 swiper prev,next btn 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,25 @@
 "use client";
 import CardFrame from "@/components/common/CardFrame";
+import { useRef, useState } from "react";
 import "swiper/css";
 import "swiper/css/free-mode";
 import { Swiper, SwiperSlide } from "swiper/react";
 import cardData from "../apis/cards.json";
-import { useState } from "react";
-import { Pagination } from "swiper/modules";
 
 const page = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const swiperRef = useRef<any>(null);
 
   return (
-    <div className="h-[46.8vw] max-h-[80vh] flex-center flex-col transition-all duration-100 ease-in">
+    <div className="relative h-[46.8vw] max-h-[80vh] flex-center flex-col transition-all duration-100 ease-in">
       <Swiper
-        className="w-full h-[66.67vw] max-h-[80%] bg-highlight "
+        className="w-full h-[66.67vw] max-h-[80%]"
         slidesPerView={3}
         centeredSlides={true}
         loop={true}
         spaceBetween={0}
         onSlideChange={swiper => setCurrentIndex(swiper.realIndex)}
+        onSwiper={swiper => (swiperRef.current = swiper)}
       >
         {cardData.map((item, index) => {
           const total = cardData.length;
@@ -45,8 +46,43 @@ const page = () => {
           );
         })}
       </Swiper>
+      <button
+        className="absolute z-50 top-[50%] right-[1%] translate-y-[-50%] h-[8%] cursor-pointer"
+        onClick={() => swiperRef.current?.slideNext()}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="text-darkgray transition-colors duration-200 w-full h-full"
+        >
+          <circle cx="12" cy="12" r="10" className="fill-light" />
+          <path d="m10 8 4 4-4 4" />
+        </svg>
+      </button>
+      <button
+        className="absolute z-50 top-[50%] left-[1%] translate-y-[-50%] h-[8%] cursor-pointer"
+        onClick={() => swiperRef.current?.slidePrev()}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="text-darkgray transition-colors duration-200 w-full h-full"
+        >
+          <circle cx="12" cy="12" r="10" className="fill-light" />
+          <path d="m14 16-4-4 4-4" />
+        </svg>
+      </button>
     </div>
   );
 };
-
 export default page;
