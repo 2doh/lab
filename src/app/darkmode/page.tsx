e574df9a01de5ae38cb6cd6a1951069fdf9385fd
2doh <ldh0973@naver.com>
2025-11-11 16:08:23 +0900

    docs : 토글 스위치 구현 comment : 다크모드 구현 필요

--- src/app/darkmode/page.tsx
+++ src/app/darkmode/page.tsx
@@ -8,18 +8,17 @@ const page = () => {
     >
       {/* input checkbox를 숨기고 label을 이용해 토글 스위치 모양 구현 */}
       <label
-        className="cursor-pointer 
+        className="cursor-pointer group
         w-full h-full max-w-[500px] max-h-[200px]"
       >
         {/* 토글 체크박스 */}
-        {/* sr-only 클래스로 input 숨김
-        peer 클래스는 상태(checked 등)를 형제 요소에 전달 */}
-        <input type="checkbox" className="peer sr-only" />
+        {/* sr-only 클래스로 input 숨김 */}
+        <input type="checkbox" className="sr-only" />
         {/* 스위치 배경 */}
         <div
-          className="w-full h-full bg-darkgray rounded-full
-          peer-checked:bg-dark-surface transition-colors duration-500
-          relative
+          className="w-full h-full bg-lightgray rounded-full
+          group-has-[input:checked]:bg-dark-surface transition-colors duration-500
+          relative shadow-[inset_1px_3px_5px_rgba(0,0,0,0.6),inset_0px_0px_5px_rgba(0,0,0,0.1)]
           "
         >
           {/* 스위치 버튼 */}
@@ -27,10 +26,47 @@ const page = () => {
             className="absolute top-[50%] left-[5%] translate-y-[-50%]
             w-[30%] h-[75%] rounded-full
             transition-transform transform duration-500
-            flex flex-center bg-amber-50
-            peer-checked:translate-x-10
+            flex flex-center bg-amber-400
+            group-has-[input:checked]:translate-x-[200%]
             "
           ></div>
+          <div className="absolute h-[60%] top-[50%] translate-y-[-50%] left-[7.8%]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="w-full h-full stroke-dark-surface
+              group-has-[input:checked]:stroke-darkgray
+              "
+            >
+              <circle cx="12" cy="12" r="4" />
+              <path d="M12 2v2" />
+              <path d="M12 20v2" />
+              <path d="m4.93 4.93 1.41 1.41" />
+              <path d="m17.66 17.66 1.41 1.41" />
+              <path d="M2 12h2" />
+              <path d="M20 12h2" />
+              <path d="m6.34 17.66-1.41 1.41" />
+              <path d="m19.07 4.93-1.41 1.41" />
+            </svg>
+          </div>
+          <div className="absolute h-[60%] top-[50%] translate-y-[-50%] right-[7.8%]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="w-full h-full stroke-darkgray
+              group-has-[input:checked]:stroke-dark-surface"
+            >
+              <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
+            </svg>
+          </div>
         </div>
       </label>
     </div>
